370fdb555658101e24069af285800eb212c863e8
jarolect <dawjarole@gmail.com>
2021-10-16 00:49:57 +0200

    Añadimos formulario para recoger datos

--- index.php
+++ index.php
@@ -1,1 +1,39 @@
-<!-- Hola esto es un comentario php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+     <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+    <title>Prestame App</title>
+</head>
+<body>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <form action="" method="POST">
+                    <input type="text" class="form-control" name="USERNAME" id="USERNAME" placeholder="Nombre de usuario">
+                    <input type="text" class="form-control" name="TELEFONO" id="TELEFONO" placeholder="Número de móvil">
+                    <input type="email" class="form-control" name="EMAIL" id="EMAIL" placeholder="Email">
+                    
+                    <input type="submit" class="btn btn-primary">
+                </form>
+            </div>
+            <div class="col-md-8"></div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
+<?php
+
+?>
+    
